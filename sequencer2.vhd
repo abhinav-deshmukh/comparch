d00407838919aa89c2b72ed1cc48056975f1111b
abhinav-deshmukh <abhinavad@gmail.com>
2012-09-26 17:56:50 +0800

    Update sequencer2.vhd

--- sequencer2.vhd
+++ sequencer2.vhd
@@ -2,6 +2,7 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.std_logic_unsigned.all;
 use work.constants.all;
+--hello
 entity sequencer2 is
     port(
 		rst                : in  std_logic;
